2f1e604a030e32b533f8ccefba884ceb2c8a89a6
Thea Kolnes <theakolnes@tjmac.lan>
2023-09-27 22:15:05 +0200

    made pageComponent for pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,75 +1,38 @@
 import { Box, SimpleGrid, IconButton, Flex, Spacer, Heading, useColorMode, useColorModeValue, LinkOverlay, LinkBox, Text } from "@chakra-ui/react"
-import { useState } from "react"
-import { IoSunny, IoMoon } from 'react-icons/io5';
-import { HiOutlineArrowSmLeft } from 'react-icons/hi'
+import PageComponent from "@/components/page";
+import { formBackgroundLight, formBackgroundDark, textColorLight, textColorDark } from "@/components/theme";
 
 const AboutPage = () => {
-  const { toggleColorMode } = useColorMode();
-  const formBackground = useColorModeValue('#D3CDF9', '#6B60B1');
-  const textColor = useColorModeValue('#404067', '#D3CDF9');
-  const [ toggle, setToggle ] =  useState(false);
-
+  const formBackground = useColorModeValue(formBackgroundLight, formBackgroundDark);
+  const textColor = useColorModeValue(textColorLight, textColorDark);
   return (
-    <Flex height='100vh' alignItems='center' justifyContent='center' background={"#272727"}>
-      <Flex direction='column' background={formBackground} p='12' rounded='6' position='relative' minW='300' maxW='350'>
-        <Heading size='md' mb='2' textAlign='center' color={textColor}>
-          About me✍🏽
-        </Heading>
-
-        <Flex>
-          <Text m='6' textAlign='center' color={textColor}>
-            I take a big interest in video editing📹, coding👩🏽‍💻 and art🎨.
-          </Text>
-        </Flex>
-
-        <Flex>
-          <Text mt='4' textAlign='center' color={textColor}>
-            <b>Projects</b>
-          </Text>
-        </Flex>
-
-        <SimpleGrid columns={1} spacingX='20px' spacingY='6px' m={4}>
-          <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.1)' }}>
-            <LinkOverlay href="/webkom">
-              <Box rounded={5} bg={textColor}>
-                <Text align='center' p='6px' color={formBackground}>Webkom💻</Text>
-              </Box>
-            </LinkOverlay>
-          </LinkBox>
-          <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.1)' }}>
-            <LinkOverlay href="">
-              <Box rounded={5} bg={textColor}>
-                <Text align='center' fontStyle='italic' p='6px' color={formBackground}>Coming…</Text>
-              </Box>
-            </LinkOverlay>
-          </LinkBox>
-        </SimpleGrid>
-        
-        <Flex>
-            <LinkBox>
-                <LinkOverlay href="/">
-                    <IconButton width='15' aria-label="previous page" icon={<HiOutlineArrowSmLeft />}  />
-                </LinkOverlay>
-            </LinkBox>
-            <Spacer/>
-            {/* <LinkBox>
-                <LinkOverlay href='/about'>
-                    <IconButton width='15' aria-label="next page" icon={<HiOutlineArrowSmRight />}  />
-                </LinkOverlay>
-            </LinkBox> */}
-            
-        </Flex>
-       
-        <Box position='absolute' top='4' right='4' cursor='pointer'onClick={() => {
-          toggleColorMode();
-          setToggle(!toggle);
-        }}
-        >
-          {toggle ? <IoSunny /> : <IoMoon /> }
-
-        </Box>
+    <PageComponent
+      header={"About me✍🏽"}
+      desc={"I take a big interest in video editing📹, coding👩🏽‍💻 and art🎨."}
+      prevPage={"/"}
+    >
+      <Flex>
+        <Text mt='4' textAlign='center' color={textColor}>
+          <b>Projects</b>
+        </Text>
       </Flex>
-    </Flex>
+      <SimpleGrid columns={1} spacingX='20px' spacingY='6px' m={4}>
+        <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.1)' }}>
+          <LinkOverlay href="/webkom">
+            <Box rounded={5} bg={textColor}>
+              <Text align='center' p='6px' color={formBackground}>Webkom💻</Text>
+            </Box>
+          </LinkOverlay>
+        </LinkBox>
+        <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.1)' }}>
+          <LinkOverlay href="/test">
+            <Box rounded={5} bg={textColor}>
+              <Text align='center' fontStyle='italic' p='6px' color={formBackground}>Tester</Text>
+            </Box>
+          </LinkOverlay>
+        </LinkBox>
+      </SimpleGrid>
+    </PageComponent>
   )
 }
 
--- src/pages/webkom.tsx
+++ src/pages/webkom.tsx
@@ -1,58 +1,27 @@
-import { Box, Flex, Heading, LinkBox, LinkOverlay, useColorMode, useColorModeValue, Text, SimpleGrid, IconButton, Spacer } from "@chakra-ui/react";
-import { useState } from "react";
-import { HiOutlineArrowSmLeft, HiOutlineHome } from "react-icons/hi";
-import { IoSunny, IoMoon } from "react-icons/io5";
+import PageComponent from "@/components/page";
+import { formBackgroundLight, formBackgroundDark, textColorLight, textColorDark } from "@/components/theme";
+import { Box, LinkBox, LinkOverlay, useColorModeValue, Text, SimpleGrid } from "@chakra-ui/react";
 
 const WebkomPage = () => {
-    const { toggleColorMode } = useColorMode();
-    const formBackground = useColorModeValue('#D3CDF9', '#6B60B1');
-    const textColor = useColorModeValue('#404067', '#D3CDF9');
-    const [ toggle, setToggle ] =  useState(false);
-
+    const formBackground = useColorModeValue(formBackgroundLight, formBackgroundDark);
+    const textColor = useColorModeValue(textColorLight, textColorDark);
     return (
-        <Flex height='100vh' alignItems='center' justifyContent='center' background={"#272727"}>
-            <Flex direction='column' background={formBackground} p='12' rounded='6' position='relative' minW='300' maxW='350'>
-                <Heading size='md' mb='4' textAlign='center'>Webkom💻</Heading>
-
-                <Text mb='5'>Webkom is a subgroup under the student organization (echo) under the Informatics Department at UIB.</Text>
-                <Text>This subgroup keeps their websites up and running.</Text>
-
-                <SimpleGrid columns={1} spacingX='20px' spacingY='6px' m={4} _hover={{textDecoration: 'underline', color: formBackground}}>
-                    <LinkBox>
-                        <LinkOverlay href="https://echo.uib.no" >
-                            <Box rounded={5} bg={textColor} >
-                            <Text align='center' fontStyle='italic' p='6px' color={formBackground}  >https://echo.uib.no</Text>
-                            </Box>
-                        </LinkOverlay>
-                    </LinkBox>
-
-                </SimpleGrid>
-
-                <Box position='absolute' top='4' right='4' cursor='pointer'onClick={() => {
-                toggleColorMode();
-                setToggle(!toggle);
-                }}
-                >
-                {toggle ? <IoSunny /> : <IoMoon /> }
-
-                </Box>
-
-                <Flex>
-                    <LinkBox>
-                        <LinkOverlay href='/about'>
-                        <IconButton width='15' aria-label="next page" icon={<HiOutlineArrowSmLeft/>}  />
-                        </LinkOverlay>
-                    </LinkBox>
-                    <Spacer/>
-                    <LinkBox mr='10'>
-                        <LinkOverlay href='/'>
-                        <IconButton width='15' aria-label="next page" icon={<HiOutlineHome />}  />
-                        </LinkOverlay>
-                    </LinkBox>
-                    <Spacer/>
-                </Flex>
-            </Flex>
-        </Flex>
+        <PageComponent
+        header="Webkom💻"
+        prevPage="/about"
+        >
+            <Text mb='5'>Webkom is a subgroup under the student organization (echo) under the Informatics Department at UIB.</Text>
+            <Text>This subgroup keeps their websites up and running.</Text>
+            <SimpleGrid columns={1} spacingY='6px' m={4} _hover={{textDecoration: 'underline', color: formBackground}}>
+                <LinkBox>
+                    <LinkOverlay href="https:echo.uib.no" >
+                        <Box rounded={5} bg={textColor} >
+                        <Text align='center' fontStyle='italic' p='6px' color={formBackground}  >https:echo.uib.no</Text>
+                        </Box>
+                    </LinkOverlay>
+                </LinkBox>
+            </SimpleGrid>
+        </PageComponent>
   )
 }
 
