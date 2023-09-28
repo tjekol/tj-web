88b3742a2ab331042e3a6bb557d30775f148a216
Thea Kolnes <thea.jenny02@gmail.com>
2023-09-29 01:54:09 +0200

    made husky executable

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -2,6 +2,5 @@
 export const textColorLight = '#404067';
 export const textColorDark = '#D3CDF9';
 
-export const formBackgroundLight = "#D3CDF9";
+export const formBackgroundLight = '#D3CDF9';
 export const formBackgroundDark = '#6B60B1';
-
