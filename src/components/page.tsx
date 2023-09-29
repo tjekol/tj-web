5ecf0f4db7e09d9694c0f7cc0d0dbba17bd8da32
Thea Kolnes <thea.jenny02@gmail.com>
2023-09-29 17:24:58 +0200

    added made by on bottom

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -49,87 +49,102 @@ const PageComponent = ({
   const [toggle, setToggle] = useState(false);
 
   return (
-    <Flex
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      background={'#272727'}
-    >
+    <>
       <Flex
-        direction="column"
-        background={formBackground}
-        p="12"
-        rounded="6"
-        position="relative"
-        minW="300"
-        maxW="350"
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+        background={'#272727'}
       >
-        <Heading size="md" mb="2" textAlign="center" color={textColor}>
-          {header}
-        </Heading>
-        {desc && (
-          <Flex>
-            <Text m="6" textAlign="center" color={textColor}>
-              {desc}
-            </Text>
-          </Flex>
-        )}
-        {children}
-        {(prevPage || nextPage) && (
-          <Flex>
-            {prevPage && (
-              <LinkBox>
-                <LinkOverlay href={prevPage}>
-                  <IconButton
-                    width="15"
-                    aria-label="previous page"
-                    icon={<HiOutlineArrowSmLeft />}
-                  />
-                </LinkOverlay>
-              </LinkBox>
-            )}
-            {homeButton && (
-              <>
-                <Spacer />
-                <LinkBox mr="10">
-                  <LinkOverlay href="/">
+        <Flex
+          direction="column"
+          background={formBackground}
+          p="12"
+          rounded="6"
+          position="relative"
+          minW="300"
+          maxW="350"
+        >
+          <Heading size="md" mb="2" textAlign="center" color={textColor}>
+            {header}
+          </Heading>
+          {desc && (
+            <Flex>
+              <Text m="6" textAlign="center" color={textColor}>
+                {desc}
+              </Text>
+            </Flex>
+          )}
+          {children}
+          {(prevPage || nextPage) && (
+            <Flex>
+              {prevPage && (
+                <LinkBox>
+                  <LinkOverlay href={prevPage}>
                     <IconButton
                       width="15"
-                      aria-label="next page"
-                      icon={<HiOutlineHome />}
+                      aria-label="previous page"
+                      icon={<HiOutlineArrowSmLeft />}
                     />
                   </LinkOverlay>
                 </LinkBox>
-              </>
-            )}
-            <Spacer />
-            {nextPage && (
-              <LinkBox>
-                <LinkOverlay href={nextPage}>
-                  <IconButton
-                    width="15"
-                    aria-label="next page"
-                    icon={<HiOutlineArrowSmRight />}
-                  />
-                </LinkOverlay>
-              </LinkBox>
-            )}
-          </Flex>
-        )}
-        <Box
-          position="absolute"
-          top="4"
-          right="4"
-          cursor="pointer"
-          onClick={() => {
-            toggleColorMode();
-            setToggle(!toggle);
-          }}
-        >
-          {toggle ? <IoSunny /> : <IoMoon />}
-        </Box>
+              )}
+              {homeButton && (
+                <>
+                  <Spacer />
+                  <LinkBox mr="10">
+                    <LinkOverlay href="/">
+                      <IconButton
+                        width="15"
+                        aria-label="next page"
+                        icon={<HiOutlineHome />}
+                      />
+                    </LinkOverlay>
+                  </LinkBox>
+                </>
+              )}
+              <Spacer />
+              {nextPage && (
+                <LinkBox>
+                  <LinkOverlay href={nextPage}>
+                    <IconButton
+                      width="15"
+                      aria-label="next page"
+                      icon={<HiOutlineArrowSmRight />}
+                    />
+                  </LinkOverlay>
+                </LinkBox>
+              )}
+            </Flex>
+          )}
+          <Box
+            position="absolute"
+            top="4"
+            right="4"
+            cursor="pointer"
+            onClick={() => {
+              toggleColorMode();
+              setToggle(!toggle);
+            }}
+          >
+            {toggle ? <IoSunny /> : <IoMoon />}
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+      <LinkBox>
+        <LinkOverlay href="https://github.com/tjekol/tj-web">
+          <Text
+            pos="relative"
+            bottom="10"
+            textAlign="center"
+            color="grey"
+            _hover={{ textDecoration: 'underline' }}
+          >
+            Made by Thea Jenny Kolnes🦋
+          </Text>
+        </LinkOverlay>
+      </LinkBox>
+    </>
   );
 };
 
