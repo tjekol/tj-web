0224191880e979d7ce310b49a0c8223100b393dc
Thea Kolnes <thea.jenny02@gmail.com>
2023-09-30 02:23:57 +0200

    added eslint remove unused imports

--- src/components/theme.ts
+++
@@ -1,6 +0,0 @@
-// theme.ts
-export const textColorLight = '#404067';
-export const textColorDark = '#D3CDF9';
-
-export const formBackgroundLight = '#D3CDF9';
-export const formBackgroundDark = '#6B60B1';
