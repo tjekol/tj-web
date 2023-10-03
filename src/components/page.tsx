b09cb3cb7a7e1991a473d78d6ecf7566c5438105
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-03 12:14:44 +0200

    💄Added transition for pages, webkom links is external

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,9 +1,3 @@
-import DarkModeSwitch, {
-  backgroundLightMode,
-  backgroundDarkMode,
-  textColorLightMode,
-  textColorDarkMode,
-} from './color-themes';
 import {
   Flex,
   Heading,
@@ -13,12 +7,20 @@ import {
   LinkBox,
   LinkOverlay,
   Spacer,
+  Box,
 } from '@chakra-ui/react';
+import DarkModeSwitch, {
+  backgroundLightMode,
+  backgroundDarkMode,
+  textColorLightMode,
+  textColorDarkMode,
+} from './color-themes';
 import {
   HiOutlineArrowSmLeft,
   HiOutlineArrowSmRight,
   HiOutlineHome,
 } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 import React from 'react';
 
 function PageComponent({
@@ -42,6 +44,15 @@ function PageComponent({
   );
   const textColor = useColorModeValue(textColorLightMode, textColorDarkMode);
 
+  const MotionBox = motion(Box);
+  const MotionHeading = motion(Heading);
+  const MotionText = motion(Text);
+
+  const pageTransition = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
+
   return (
     <>
       <Flex
@@ -50,71 +61,83 @@ function PageComponent({
         justifyContent='center'
         background={'#272727'}
       >
-        <Flex
-          position='relative'
-          paddingY='8'
-          paddingX='6'
-          direction='column'
-          background={formBackground}
-          rounded='6'
-          minW='350'
-          maxW='320'
-          minH='300'
+        <MotionBox
+          variants={pageTransition}
+          initial='initial'
+          animate='animate'
+          exit='initial'
         >
-          <Heading size='md' mb='2' textAlign='center' color={textColor}>
-            {header}
-          </Heading>
-          {desc && (
-            <Flex>
-              <Text m='6' textAlign='center' color={textColor}>
-                {desc}
-              </Text>
-            </Flex>
-          )}
-          {children}
-          {(prevPage || nextPage) && (
-            <Flex>
-              {prevPage && (
-                <LinkBox>
-                  <LinkOverlay href={prevPage}>
-                    <IconButton
-                      width='15'
-                      aria-label='previous page'
-                      icon={<HiOutlineArrowSmLeft />}
-                    />
-                  </LinkOverlay>
-                </LinkBox>
-              )}
-              {homeButton && (
-                <>
-                  <Spacer />
-                  <LinkBox mr='10'>
-                    <LinkOverlay href='/'>
+          <Flex
+            position='relative'
+            paddingY='8'
+            paddingX='6'
+            direction='column'
+            background={formBackground}
+            rounded='6'
+            minW='350'
+            maxW='320'
+            minH='300'
+          >
+            <MotionHeading
+              size='md'
+              mb='2'
+              textAlign='center'
+              color={textColor}
+            >
+              {header}
+            </MotionHeading>
+            {desc && (
+              <Flex>
+                <MotionText m='6' textAlign='center' color={textColor}>
+                  {desc}
+                </MotionText>
+              </Flex>
+            )}
+            {children}
+            {(prevPage || nextPage) && (
+              <Flex>
+                {prevPage && (
+                  <LinkBox>
+                    <LinkOverlay href={prevPage}>
                       <IconButton
                         width='15'
-                        aria-label='next page'
-                        icon={<HiOutlineHome />}
+                        aria-label='previous page'
+                        icon={<HiOutlineArrowSmLeft />}
                       />
                     </LinkOverlay>
                   </LinkBox>
-                </>
-              )}
-              <Spacer />
-              {nextPage && (
-                <LinkBox>
-                  <LinkOverlay href={nextPage}>
-                    <IconButton
-                      width='15'
-                      aria-label='next page'
-                      icon={<HiOutlineArrowSmRight />}
-                    />
-                  </LinkOverlay>
-                </LinkBox>
-              )}
-            </Flex>
-          )}
-          <DarkModeSwitch />
-        </Flex>
+                )}
+                {homeButton && (
+                  <>
+                    <Spacer />
+                    <LinkBox mr='10'>
+                      <LinkOverlay href='/'>
+                        <IconButton
+                          width='15'
+                          aria-label='next page'
+                          icon={<HiOutlineHome />}
+                        />
+                      </LinkOverlay>
+                    </LinkBox>
+                  </>
+                )}
+                <Spacer />
+                {nextPage && (
+                  <LinkBox>
+                    <LinkOverlay href={nextPage}>
+                      <IconButton
+                        width='15'
+                        aria-label='next page'
+                        icon={<HiOutlineArrowSmRight />}
+                      />
+                    </LinkOverlay>
+                  </LinkBox>
+                )}
+              </Flex>
+            )}
+            <DarkModeSwitch />
+          </Flex>
+        </MotionBox>
       </Flex>
       <LinkBox>
         <LinkOverlay href='https://github.com/tjekol/tj-web'>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Spacer, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Image, Spacer, useColorModeValue, Box } from '@chakra-ui/react';
 import LanguageSwitcher from '@/components/language-switcher';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import useLanguage from '@/hooks/use-language';
@@ -6,54 +6,75 @@ import LinkIcon from '@/components/link-icon';
 import PageComponent from '@/components/page';
 import thea from '@/assets/cv-bilde.jpeg';
 import { MdEmail } from 'react-icons/md';
+import { motion } from 'framer-motion';
 import React from 'react';
 
 function IndexPage() {
   const textColor = useColorModeValue('#404067', '#D3CDF9');
   const isNorwegian = useLanguage();
+  const MotionBox = motion(Box);
+
+  const pageTransition = {
+    initial: { y: -10 },
+    animate: { y: 0 },
+  };
 
   return (
-    <PageComponent
-      header={isNorwegian ? '✨HEI DU✨' : '✨HI THERE✨'}
-      desc={
-        isNorwegian
-          ? ' Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i Bergen på UIB.'
-          : 'My name is Thea Jenny Kolnes. I am currently studying Computer Technology💻 in Bergen at UIB.'
-      }
-      nextPage='/about'
+    <Flex
+      height='100vh'
+      alignItems='center'
+      justifyContent='center'
+      background={'#272727'}
     >
-      <Image
-        src={thea.src}
-        alignSelf='center'
-        borderRadius='full'
-        boxSize='150px'
-        fallbackSrc='https://via.placeholder.com/150'
-        alt='Bilde av meg'
-      />
-      <Flex mt='2' direction='row'>
-        <Spacer />
-        <LinkIcon
-          link={'https://www.linkedin.com/in/thea-jenny-kolnes-a79821231/'}
-          label={'Linkedin-en min'}
-          color={textColor}
-          icon={FaLinkedin}
-        />
-        <LinkIcon
-          link={'https://github.com/tjekol'}
-          label={'Githubben min'}
-          color={textColor}
-          icon={FaGithub}
-        />
-        <LinkIcon
-          link={'mailto:thea.jenny02@gmail.com'}
-          label={'Send mail til meg'}
-          color={textColor}
-          icon={MdEmail}
-        />
-        <Spacer />
-      </Flex>
-      <LanguageSwitcher />
-    </PageComponent>
+      <MotionBox
+        variants={pageTransition}
+        initial='initial'
+        animate='animate'
+        exit='initial'
+      >
+        <PageComponent
+          header={isNorwegian ? '✨HEI DU✨' : '✨HI THERE✨'}
+          desc={
+            isNorwegian
+              ? ' Mitt navn er Thea Jenny Kolnes. Jeg tar bachelor i Datateknologi💻 i Bergen på UIB.'
+              : 'My name is Thea Jenny Kolnes. I am currently studying Computer Technology💻 in Bergen at UIB.'
+          }
+          nextPage='/about'
+        >
+          <Image
+            src={thea.src}
+            alignSelf='center'
+            borderRadius='full'
+            boxSize='150px'
+            fallbackSrc='https://via.placeholder.com/150'
+            alt='Bilde av meg'
+          />
+          <Flex mt='2' direction='row'>
+            <Spacer />
+            <LinkIcon
+              link={'https://www.linkedin.com/in/thea-jenny-kolnes-a79821231/'}
+              label={'Linkedin-en min'}
+              color={textColor}
+              icon={FaLinkedin}
+            />
+            <LinkIcon
+              link={'https://github.com/tjekol'}
+              label={'Githubben min'}
+              color={textColor}
+              icon={FaGithub}
+            />
+            <LinkIcon
+              link={'mailto:thea.jenny02@gmail.com'}
+              label={'Send mail til meg'}
+              color={textColor}
+              icon={MdEmail}
+            />
+            <Spacer />
+          </Flex>
+          <LanguageSwitcher />
+        </PageComponent>
+      </MotionBox>
+    </Flex>
   );
 }
 
